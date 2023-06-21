f0513381ff03717080c27a63fb18709398a12dfd
msrbl <msrbl@github.com>
2023-06-21 21:10:43 +0500

    Лаба

---
+++ ARVR Lab1/Assets/CheckCollision.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.name == "Cube")
+        {
+            other.gameObject.GetComponent<Renderer>().material.SetColor("_Сolor", Color.green);
+        }
+
+    }
+
+    private void OnCollisionExit(Collision other) {
+        if (other.gameObject.name == "Cube")
+        {
+            other.gameObject.GetComponent<Renderer>().material.SetColor("_Сolor", Color.red);
+        }
+
+    }
+}
